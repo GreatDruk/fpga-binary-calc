62dd26b40e80a333bae669dd23277af435acd92f
Seva Peretiatko <peretyatkosewa06@mail.ru>
2026-03-08 01:25:12 +0300

    style(adder): fix margins

--- src/arithmetic/full_adder.sv
+++ src/arithmetic/full_adder.sv
@@ -1,12 +1,12 @@
 module full_adder(
-		input cin, a, b,
-		output q, cout
+	input cin, a, b,
+	output q, cout
 );
-		wire w, c1, c2;
-		
-		half_adder ha1 (.a(a), .b(b), .q(w),  .cout(c1));
-				
-		half_adder ha2 (.a(cin), .b(w), .q(q), .cout(c2));
+	wire w, c1, c2;
 
-		assign cout = c1 | c2;
+	half_adder ha1 (.a(a), .b(b), .q(w),  .cout(c1));
+			
+	half_adder ha2 (.a(cin), .b(w), .q(q), .cout(c2));
+
+	assign cout = c1 | c2;
 endmodule
