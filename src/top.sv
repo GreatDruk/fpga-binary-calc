1f9a59e16e478cc11b10a5be2a578262f502d692
Seva Peretiatko <peretyatkosewa06@mail.ru>
2026-03-11 00:11:05 +0300

    feat(top): make declaration of var

--- src/top.sv
+++ src/top.sv
@@ -4,5 +4,31 @@ module top(
 	output logic [17:0] display_value
 );
 
+	// Calculator states
+	typedef enum logic [1:0] {
+		ENTER_A,
+		ENTER_OPCODE,
+		ENTER_B,
+		SHOW_RESULT
+	} state_t;
+
+	state_t state, next_state;
+
+	// Input
+	logic [8:0] A;
+	logic [8:0] B;
+	logic [1:0] opcode;
+
+	// Output
+	logic [8:0] sum;
+	logic cout;
+	logic [8:0] diff;
+	logic [17:0] product;
 	
+	// Result
+	logic [17:0] result;
+
+
+	
+
 endmodule
