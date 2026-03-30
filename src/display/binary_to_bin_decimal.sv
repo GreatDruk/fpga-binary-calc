a055b1853c9951ecdaa520a7534ef14ef4f25f2f
Seva Peretiatko <peretyatkosewa06@mail.ru>
2026-03-30 13:11:58 +0300

    fix(display): fix constant size error

--- src/display/binary_to_bin_decimal.sv
+++ src/display/binary_to_bin_decimal.sv
@@ -7,18 +7,20 @@ module binary_to_bin_decimal(
 	logic [41:0] shift;
 
 	always_comb begin
-		shift = 0;
+		shift = 42'b0;
 		shift[17:0] = binary;
-
+		
 		for(i = 0; i < 18; i++) begin
-			if(shift[21:18] >= 5) shift[21:18] += 3;
-			if(shift[25:22] >= 5) shift[25:22] += 3;
-			if(shift[29:26] >= 5) shift[29:26] += 3;
-			if(shift[33:30] >= 5) shift[33:30] += 3;
-			if(shift[37:34] >= 5) shift[37:34] += 3;
-			if(shift[41:38] >= 5) shift[41:38] += 3;
+	
+			if(shift[21:18] >= 4'd5) shift[21:18] += 4'd3;
+			if(shift[25:22] >= 4'd5) shift[25:22] += 4'd3;
+			if(shift[29:26] >= 4'd5) shift[29:26] += 4'd3;
+			if(shift[33:30] >= 4'd5) shift[33:30] += 4'd3;
+			if(shift[37:34] >= 4'd5) shift[37:34] += 4'd3;
+			if(shift[41:38] >= 4'd5) shift[41:38] += 4'd3;
 
 			shift = shift << 1;
+
 		end
 
 		bin_decimal = shift[41:18];
